df08bb48758fa220826583c03ed243e4279c054a
Louise Collins <louisecollins1971@icloud.com>
2025-12-30 01:10:51 +0000

    Using Input Actions

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+    }
+
+    private void Update()
+    {
+        if (thrust.IsPressed())
+
+        {
+            Debug.Log ("thrusting");
+        }
+    }
+}
