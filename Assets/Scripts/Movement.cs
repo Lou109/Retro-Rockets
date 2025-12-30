911e914fd62ce22a9d8ba3b70ef51187b65c7b11
Louise Collins <louisecollins1971@icloud.com>
2025-12-30 13:37:37 +0000

    Rotation Input

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,8 +4,9 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] float thrustStrength = 100f;
-
+    
     Rigidbody rb;
 
     void Start()
@@ -16,14 +17,26 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     private void FixedUpdate()
     {
-        if (thrust.IsPressed())
+        ProcessThrust();
+        ProcessRotation();
+    }
 
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
         {
-            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+            rb.AddRelativeForce(thrustStrength * Time.fixedDeltaTime * Vector3.up);
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log ("Here is our rotation value: " + rotationInput);
     }
 }
