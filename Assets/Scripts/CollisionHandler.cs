59e3e3e8be1feb68076136fc37f1a2fc50c87587
Louise Collins <louisecollins1971@icloud.com>
2026-01-01 19:34:15 +0000

    Switch Statements

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag) 
+        {
+            
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Fuel":
+                Debug.Log("You found fuel!");
+                Destroy(other.gameObject);
+                break;
+            case "Finish":
+                Debug.Log("You won this level!");
+                break;
+            default:
+                Debug.Log("You hit something dangerous!");
+                break;
+        }
+    }
+}
